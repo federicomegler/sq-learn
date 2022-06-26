8a5ef4fe821788f2ac9d0433ebd2e684d59e3315
federico.megler <56395003+federicomegler@users.noreply.github.com>
2022-06-26 10:12:09 +0200

    Implemented LSSVC class

--- sklearn/svm/__init__.py
+++ sklearn/svm/__init__.py
@@ -10,7 +10,7 @@ The :mod:`sklearn.svm` module includes Support Vector Machine algorithms.
 #         of their respective owners.
 # License: BSD 3 clause (C) INRIA 2010
 
-from ._classes import SVC, NuSVC, SVR, NuSVR, OneClassSVM, LinearSVC, \
+from ._classes import SVC, NuSVC, SVR, NuSVR, OneClassSVM, LSSVC, LinearSVC, \
         LinearSVR
 from ._bounds import l1_min_c
 
@@ -21,4 +21,5 @@ __all__ = ['LinearSVC',
            'OneClassSVM',
            'SVC',
            'SVR',
+           'LSSVC',
            'l1_min_c']
--- sklearn/svm/_classes.py
+++ sklearn/svm/_classes.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 from ._base import _fit_liblinear, BaseSVC, BaseLibSVM
-from ..base import BaseEstimator, RegressorMixin, OutlierMixin
+from ..base import BaseEstimator, ClassifierMixin, RegressorMixin, OutlierMixin
 from ..linear_model._base import LinearClassifierMixin, SparseCoefMixin, \
     LinearModel
-from ..utils.validation import _num_samples
+from ..utils.validation import _num_samples, check_X_y, check_array, check_is_fitted
 from ..utils.validation import _deprecate_positional_args
 from ..utils.multiclass import check_classification_targets
 from ..utils.deprecation import deprecated
+from ..metrics.pairwise import linear_kernel, rbf_kernel, polynomial_kernel, sigmoid_kernel
 
 
 class LinearSVC(LinearClassifierMixin,
@@ -1458,3 +1459,155 @@ class OneClassSVM(OutlierMixin, BaseLibSVM):
                 'zero sample_weight is not equivalent to removing samples',
             }
         }
+
+
+class LSSVC(BaseEstimator):
+    """
+    LSSVC implementation. 
+    "Least squares Support Vector Machine Classifier" Suykens, Vandewalle
+
+    Parameters
+    ----------
+
+    kernel : {'linear', 'poly', 'rbf', 'sigmoid'}, default='linear'
+         Specifies the kernel type to be used in the algorithm.
+         It must be one of 'linear', 'poly', 'rbf', 'sigmoid'.
+         If none is given, 'linear' will be used.
+
+    degree : int, default=3
+        Degree of the polynomial kernel function ('poly').
+        Ignored by all other kernels.
+
+    gamma : {'scale', 'auto'} or float, default='scale'
+        Kernel coefficient for 'rbf', 'poly' and 'sigmoid'.
+
+        - if ``gamma='scale'`` (default) is passed then it uses
+          1 / (n_features * X.var()) as value of gamma,
+        - if 'auto', uses 1 / n_features.
+
+        .. versionchanged:: 0.22
+           The default value of ``gamma`` changed from 'auto' to 'scale'.
+
+    coef0 : float, default=0.0
+        Independent term in kernel function.
+        It is only significant in 'poly' and 'sigmoid'.
+
+    penalty: float, default=0.1
+        relative weight of the training error.
+    """
+
+    def __init__(self, kernel='linear', penalty=0.1, degree=3, gamma='scale', coef0=0.0) -> None:
+        super().__init__()
+        self.kernel = kernel
+        self.penalty = penalty
+        self.degree = degree
+        self.gamma = gamma
+        self.coef0 = coef0
+        
+        self.alpha = None
+        self.b = None
+        self.is_fitted_ = False
+        self.X = None
+
+
+    def fit(self, X, y):
+        """Fit the model according to the given training data.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Training vector, where n_samples in the number of samples and
+            n_features is the number of features.
+
+        y : array-like of shape (n_samples,)
+            Target vector relative to X.
+
+        Returns
+        -------
+        self : object
+            An instance of the estimator.
+        """
+
+        #X, y = self._validate_data(X, y)
+
+        self.X = X
+
+        M = len(X)
+        #construction of matrix F
+        Z = self.get_kernel(X) + (self.penalty ** -1) * np.identity(M)
+        
+        F = np.block([[0, np.ones((1,M))],
+                      [np.ones((M,1)), Z]])
+        
+        
+        # solution is [b a] = F^-1 * [0 y]
+        y = np.concatenate(([0], y))
+
+        F_inv = np.linalg.pinv(F)
+
+        sol = np.dot(F_inv, y)
+
+        self.b = sol[0]
+        self.alpha = sol[1:]
+
+        self.is_fitted_ = True
+
+        return self
+        
+
+    
+    def predict(self, X):
+        """Perform classification on samples in X.
+
+        For a one-class model, +1 or -1 is returned.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features) or \
+                (n_samples_test, n_samples_train)
+            For kernel="precomputed", the expected shape of X is
+            (n_samples_test, n_samples_train).
+
+        Returns
+        -------
+        y_pred : ndarray of shape (n_samples,)
+            Class labels for samples in X.
+        """
+
+        check_array(X)
+        check_is_fitted(self)
+
+        y_pred = np.zeros(len(X))
+        for i in range(len(X)):
+            y_pred[i] = np.sign(np.dot(self.alpha, self.get_kernel([X[i][:]], self.X)[0]) + self.b)
+
+        return y_pred
+
+
+    
+    def get_kernel(self, X, Y=None):
+        if self.kernel == 'linear':
+            return linear_kernel(X=X, Y=Y)
+
+        elif self.kernel == 'poly':
+            gamma = self._get_gamma()
+            return polynomial_kernel(X=X, Y=Y, degree=self.degree, gamma=gamma, coef0=self.coef0)
+        
+        elif self.kernel == 'rbf':
+            gamma = self._get_gamma()
+            return rbf_kernel(X=X, Y=Y, gamma=gamma)
+
+        elif self.kernel == 'sigmoid':
+            gamma = self._get_gamma()
+            return sigmoid_kernel(X=X, Y=Y, gamma=gamma, coef0=self.coef0)
+
+
+
+    def _get_gamma(self, X):
+        gamma=None
+        if self.gamma == 'scale':
+            gamma = 1 / (X.shape[1] * X.var())
+        elif self.gamma == 'auto':
+            gamma = 1 / self.n_features_in_
+        
+        return gamma
